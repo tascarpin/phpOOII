1fa97b511d447b94ff799c1297352252cc8acaa2
tascarpin <tascarpin@yahoo.com.br>
2019-09-05 20:34:44 -0300

    Alteração da ProdutoController

---
+++ controller/ProdutoController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tassio Pinheiro
+ * Date: 01/09/2019
+ * Time: 19:36
+ */
+
+namespace controller;
+
+use model\Model as Model;
+use dataBase\Conexao as Conexao;
+use model\Produto as Produto;
+
+abstract class ProdutoController implements Controller
+{
+    public function index(){
+        echo "View index de produto";
+    }
+
+    public function criar($produto){
+        mysqli_query(Conexao::conectar(), Model::criar($produto));
+        Conexao::desconectar();
+    }
+
+    public function listar($condicao = array()){
+        return mysqli_fetch_all(mysqli_query(Conexao::conectar(),Model::listar( new Produto(), $condicao)));
+        Conexao::desconectar();
+    }
+
+    public function editar($atributos = array(), $condicao = array()){
+        mysqli_query(Conexao::conectar(), Model::editar('produto', $atributos, $condicao));
+        Conexao::desconectar();
+    }
+
+    public function deletar($condicao = array()){
+            mysqli_query(Conexao::conectar(),Model::deletar('produto', $condicao));
+        Conexao::desconectar();
+    }
+
+}
