02cf86eceae2e064f22546936e6b953b4971d534
tascarpin <tascarpin@yahoo.com.br>
2019-09-12 19:40:40 -0300

    Criando rotas com Slim.

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -11,7 +11,40 @@ namespace controller;
 abstract class HomeController
 {
     public function index(){
+<<<<<<< Updated upstream
         echo "Index home";
 //        return __DIR__  . "/../view/index.php";
+=======
+<<<<<<< Updated upstream
+        require ('../resources/view/home/home.html');
+=======
+        echo "Index home";
+//        $data = array('name' => 'Rob', 'age' => 40);
+//        $payload = json_encode($data);
+//
+//        $response->getBody()->write($payload);
+//        return $response
+//            ->withHeader('Content-Type', 'application/json')
+//            ->withStatus(201);
+//        require ('../resources/view/home/home.html');
+    }
+
+    public function listar(){
+        require ('../resources/view/home/listaProdutos.html');
+    }
+
+    public function sobre(){
+        require ('../resources/view/home/sobre.html');
+    }
+
+    public function ajuda(){
+        require ('../resources/view/home/ajuda.html');
+    }
+
+    public function login(){
+        require ('../resources/view/home/login.html');
+//         header('Location: login');
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     }
 }
--- public/index.php
+++ public/index.php
@@ -6,5 +6,17 @@
  * Time: 12:17
  */
 
+<<<<<<< Updated upstream
 require  __DIR__ . "/../bootstrap/autoload.php";
 require_once  __DIR__ . "/../bootstrap/App.php";
+=======
+<<<<<<< Updated upstream
+require  "../bootstrap/autoload.php";
+require "../configuracoes/rotas.php";
+require_once  "../bootstrap/App.php";
+=======
+require "../bootstrap/autoload.php";
+require_once "../bootstrap/app.php";
+
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
